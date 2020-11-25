e63b6fdc3e10eb377afd9f85ef572e81dc274aeb
Colin Rofls <colin@cmyr.net>
2020-11-25 14:25:14 -0500

    Cleanup formatter work
    This resolves a bunch of fixmes and loose ends

--- druid/examples/flex.rs
+++ druid/examples/flex.rs
@@ -14,14 +14,13 @@
 
 //! Demonstrates alignment of children in the flex container.
 
+use druid::text::format::ParseFormatter;
 use druid::widget::prelude::*;
 use druid::widget::{
     Button, Checkbox, CrossAxisAlignment, Flex, Label, MainAxisAlignment, ProgressBar, RadioGroup,
     SizedBox, Slider, Stepper, Switch, TextBox, WidgetExt,
 };
-use druid::{
-    AppLauncher, Color, Data, Lens, LensExt, LocalizedString, PlatformError, WidgetId, WindowDesc,
-};
+use druid::{AppLauncher, Color, Data, Lens, LocalizedString, PlatformError, WidgetId, WindowDesc};
 
 const DEFAULT_SPACER_SIZE: f64 = 8.;
 
@@ -210,11 +209,8 @@ fn make_spacer_select() -> impl Widget<Params> {
             Flex::row()
                 .with_child(
                     TextBox::new()
-                        .parse()
-                        .lens(
-                            Params::spacer_size
-                                .map(|x| Some(*x), |x, y| *x = y.unwrap_or(DEFAULT_SPACER_SIZE)),
-                        )
+                        .with_formatter(ParseFormatter::new())
+                        .lens(Params::spacer_size)
                         .fix_width(60.0),
                 )
                 .with_spacer(druid::theme::WIDGET_CONTROL_COMPONENT_PADDING)
--- druid/src/core.rs
+++ druid/src/core.rs
@@ -1094,6 +1094,7 @@ impl WidgetState {
 mod tests {
     use super::*;
     use crate::ext_event::ExtEventHost;
+    use crate::text::format::ParseFormatter;
     use crate::widget::{Flex, Scroll, Split, TextBox};
     use crate::{WidgetExt, WindowHandle, WindowId};
 
@@ -1103,13 +1104,13 @@ mod tests {
 
     #[test]
     fn register_children() {
-        fn make_widgets() -> impl Widget<Option<u32>> {
+        fn make_widgets() -> impl Widget<u32> {
             Split::columns(
-                Flex::<Option<u32>>::row()
-                    .with_child(TextBox::new().with_id(ID_1).parse())
-                    .with_child(TextBox::new().with_id(ID_2).parse())
-                    .with_child(TextBox::new().with_id(ID_3).parse()),
-                Scroll::new(TextBox::new().parse()),
+                Flex::<u32>::row()
+                    .with_child(TextBox::new().with_formatter(ParseFormatter).with_id(ID_1))
+                    .with_child(TextBox::new().with_formatter(ParseFormatter).with_id(ID_2))
+                    .with_child(TextBox::new().with_formatter(ParseFormatter).with_id(ID_3)),
+                Scroll::new(TextBox::new().with_formatter(ParseFormatter)),
             )
         }
 
@@ -1136,7 +1137,7 @@ mod tests {
 
         let env = Env::default();
 
-        widget.lifecycle(&mut ctx, &LifeCycle::WidgetAdded, &None, &env);
+        widget.lifecycle(&mut ctx, &LifeCycle::WidgetAdded, &1, &env);
         assert!(ctx.widget_state.children.may_contain(&ID_1));
         assert!(ctx.widget_state.children.may_contain(&ID_2));
         assert!(ctx.widget_state.children.may_contain(&ID_3));
